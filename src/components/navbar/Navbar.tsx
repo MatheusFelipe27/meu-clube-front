39870acdcdae8dac53d31f2f5b5e75996b4f8a4d
Matheus Felipe <mfs5@cin.ufpe.br>
2023-09-06 04:07:12 -0300

    feat implement fixed navbar on scroll

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.scss'
 import logo from './../../assets/logo.png'
 
 const Navbar = () => {
+  const [navbarFixed,setNavbarFixed] = useState<boolean>(false)
+
+  const updateNavbarPosition = () => {
+      window.scrollY > 0 ? setNavbarFixed(true) : setNavbarFixed(false);
+  };
+
+  useEffect(()=>{
+      window.addEventListener("scroll", updateNavbarPosition);
+      return () => {
+        window.removeEventListener("scroll", updateNavbarPosition);
+      };
+  },[])
   return (
     <>
         <nav className='nav'>
             <div className='navLeft'>
               <img className='navLeftLogo' src={logo} alt='logo'/>
-              <span> FC Matheus</span>
+              <span id={navbarFixed? 'teamNameFixed' : 'teamName'}> FC Matheus</span>
             </div>
             <div className='navRight'>
-              <span> Inicio</span>
-              <span> Departamentos</span>
+              <a href='#homeInitial' id={navbarFixed? 'sectionsNameFixed' : 'sectionsName'}> Inicio</a>
+              <a href='#homeDepartments' id={navbarFixed? 'sectionsNameFixed' : 'sectionsName'}> Departamentos</a>
             </div>
         </nav>
     </>
